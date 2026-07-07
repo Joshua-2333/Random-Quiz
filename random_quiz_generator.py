55758a703a5b3d67e74086eb1417a34ffdbc4fd6
Joshua-2333 <jaflores.2333@gmail.com>
2026-07-07 16:23:13 -0500

    Add question generation and answer options for capitals quiz

--- random_quiz_generator.py
+++ random_quiz_generator.py
@@ -1,24 +1,25 @@
 import random
 
 CAPITALS = {
-	'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-	'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
-	'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
-	'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
-	'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
-	'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
-	'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
-	'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
-	'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
-	'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
-	'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-	'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-	'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-	'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville',
-	'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier',
-	'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
-	'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne',
+    'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+    'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+    'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
+    'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
+    'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville',
+    'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier',
+    'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne',
 }
+
 
 for quiz_num in range(35):
 
@@ -43,7 +44,36 @@ for quiz_num in range(35):
         f"State Capitals Quiz (Form {quiz_num + 1})\n\n"
     )
 
-    answer_file.write("Answer Key\n\n")
+    states = list(CAPITALS.keys())
+    random.shuffle(states)
+
+
+    for question_num in range(50):
+
+        state = states[question_num]
+        correct_answer = CAPITALS[state]
+
+        wrong_answers = list(CAPITALS.values())
+        wrong_answers.remove(correct_answer)
+
+        wrong_answers = random.sample(wrong_answers, 3)
+
+        answer_options = wrong_answers + [correct_answer]
+
+        random.shuffle(answer_options)
+
+        quiz_file.write(
+            f"{question_num + 1}. What is the capital of {state}?\n"
+        )
+
+        letters = ["A", "B", "C", "D"]
+
+        for i in range(4):
+            quiz_file.write(
+                f"    {letters[i]}. {answer_options[i]}\n"
+            )
+
+        quiz_file.write("\n")
 
     quiz_file.close()
     answer_file.close()
